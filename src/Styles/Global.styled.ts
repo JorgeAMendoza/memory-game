4758e59b2cce724837c7a105ae12c303fcebece3
Jorge Mendoza <jorgemendoza2002@gmail.com>
2022-06-18 12:58:14 -0500

    created global stylesheet for the application with styled components
    global stylesheet created for appliation, also created variables sheets containing color values and those values applied to their use

---
+++ src/Styles/Global.styled.ts
@@ -0,0 +1,55 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    font-size: 62.5%;
+    box-sizing: border-box;
+  }
+  *,
+  *::after,
+  *::before {
+    box-sizing: inherit;
+    padding: 0;
+    margin: 0;
+    line-height: 1.4;
+  }
+  body{
+    font-family: 'Outfit', sans-serif;
+    font-size: 1.6rem;
+    min-height: 100vh;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #1A2A33;
+    position:relative;
+    #root{
+      width: 90%;
+      max-width: 46rem;
+      margin: 0 auto;
+    }
+  } 
+  img,svg {
+    max-width: 100%;
+    display: block;
+  }
+  input {
+    font-family: inherit;
+    border: none;
+  }
+  ul{
+    list-style: none;
+  }
+  p,
+  li,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    overflow-wrap: break-word;
+    hyphens: auto;
+  }
+`;
+
+export default GlobalStyles;
---
+++ src/Styles/variables.ts
@@ -0,0 +1,74 @@
+const colors = {
+  blue: {
+    dark: '#304859',
+    darker: '#152938',
+    greyish: '#BCCED9',
+    pale: '#7191A5',
+    paleLight: '#6395B8',
+  },
+  white: {
+    light: '#FCFCFC',
+    greyish: '#F2F2F2',
+    pale: '#DFE7EC',
+  },
+  orange: {
+    neon: '#FDA214',
+    light: '#FFB84A',
+  },
+};
+
+const gameColors = {
+  backgroundColor: colors.blue.dark, //background color of the application
+  blueButton: {
+    background: colors.blue.dark,
+    backgroundDisabled: colors.blue.greyish,
+    backgroundHover: colors.blue.paleLight,
+    textColor: colors.white.light,
+  },
+  orangeButton: {
+    background: colors.orange.neon,
+    backgroundHover: colors.orange.light,
+    textColor: colors.white.light,
+  },
+  restartButton: {
+    background: colors.orange.neon,
+    backgrounHover: colors.orange.light,
+    textColor: colors.white.light,
+  },
+  greyButton: {
+    background: colors.white.pale,
+    backgroundHover: colors.blue.paleLight,
+    textColor: colors.blue.dark,
+    textColorHover: colors.white.light,
+  },
+  gameTile: {
+    cover: colors.blue.dark,
+    coverHover: colors.blue.paleLight,
+    background: colors.blue.greyish,
+    backgroundActive: colors.orange.neon,
+  },
+  scoreCard: {
+    background: colors.white.pale,
+    backgroundActive: colors.orange.neon,
+    textColorActive: colors.white.light,
+    textColorLight: colors.blue.pale,
+    textColorDark: colors.blue.dark,
+    textColorCurrent: colors.blue.darker,
+  },
+  gameOverModal: {
+    background: colors.white.light,
+    winningTextColor: colors.blue.darker,
+    subtextColor: colors.blue.pale,
+    playerWinBackground: colors.blue.darker,
+    playerLoseBackground: colors.white.pale,
+    playerWinTextColor: colors.white.light,
+    playerLoseNameTextColor: colors.blue.paleLight,
+    playerLoseScoreTextColor: colors.blue.dark,
+    movesTextColor: colors.blue.dark,
+    timeElapsedColor: colors.blue.dark,
+    timeElapsedBackground: colors.white.greyish,
+    singleGameStatsColor: colors.blue.paleLight,
+  },
+};
+
+export default gameColors;
